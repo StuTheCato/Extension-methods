db1ea4c730566cc523fa889db47351c8a5523ae2
StuTheCato <stuat.@Stuats-MacBook-Air.local>
2024-02-10 18:18:45 +0100

    Initial commit

---
+++ Erweiterungsmethoden/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Test".Reverse());
+    }
+}
+
+public static class StringExtension
+{
+    public static string Reverse(this string original)
+    {
+        string reverseString = null;
+        for (int i = original.Length - 1; i >= 0; i--)
+        
+            reverseString = reverseString + original[i];
+        return reverseString;
+    }
+}
